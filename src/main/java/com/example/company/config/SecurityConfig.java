7e5a44a16181106c450a771ae97b2d9a76c0e90d
Vlana Kim <vlanakim@Noutbuk-Vlana.local>
2025-11-12 16:31:10 +0300

    Add basic Spring Security configuration

---
+++ src/main/java/com/example/company/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.company.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/home", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var users = new InMemoryUserDetailsManager(
+                User.withUsername("user").password("{noop}password").roles("USER").build(),
+                User.withUsername("admin").password("{noop}admin").roles("ADMIN").build()
+        );
+        return users;
+    }
+}
